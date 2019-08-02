5ef02437590b08e1dc6c3a4999975b24b815da56
Bronegan <48139340+Bronegan@users.noreply.github.com>
2019-08-02 09:06:13 -0400

    Create countingPrimes.cpp

---
+++ countingPrimes.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+	int userInput;
+	bool isPrime = true;
+	int nextVal = 11;
+	vector<int> primes{2,3,5,7};
+	
+	cout<<"Please enter how many primes you would like to find: "
+	cin >> userInput;
+	
+	while(userInput > primes.size()){
+		for(int i=0; i<primes.size(); i++){
+			if(nextVal%primes[i]==0){
+				isPrime = false;
+				break;
+			}
+			else if(primes[i] > nextVal / 2){
+				break;
+			}
+		}
+		if(isPrime){
+			primes.push_back(nextVal);
+			cout<<(float(primes.size())/float(userInput)*100.0) << "% Complete" << endl;
+		}
+		else{
+			isPrime = true;
+		}
+		nextVal++;
+	}
+	
+	for(int i=0; i<primes.size(); i++){
+		cout << primes[i]<<" ";
+	}
+	cout << endl << "Primes Found:  " << userInput<<endl;
+	return 0;
+}
